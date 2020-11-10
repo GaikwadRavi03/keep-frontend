824ab7ac96db6890ffa00c3da8edd9d4c473c84e
GaikwadRavi03 <55272071+GaikwadRavi03@users.noreply.github.com>
2020-11-10 18:57:59 +0530

    initial commit

---
+++ build/precache-manifest.6df3e3431887c0b111e4c139213aaebd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f5ee01c62e35936450c2a17f767abd51",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5236b8afa153706a06e5",
+    "url": "/static/css/main.7a2b5803.chunk.css"
+  },
+  {
+    "revision": "076132238b67c745c9e9",
+    "url": "/static/js/2.fbd9dd52.chunk.js"
+  },
+  {
+    "revision": "425f71bb909b45cb21829b79476852ea",
+    "url": "/static/js/2.fbd9dd52.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5236b8afa153706a06e5",
+    "url": "/static/js/main.9a7347d5.chunk.js"
+  },
+  {
+    "revision": "4390a351b0f90a7c34c6",
+    "url": "/static/js/runtime-main.8bc1365d.js"
+  }
+]);
